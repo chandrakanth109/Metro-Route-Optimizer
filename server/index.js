4ab9a6f46f5bba77f572ff94e99dad709453a048
chandrakanth109 <chandumallela007@gmail.com>
2026-06-24 16:11:21 +0530

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -3,13 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type"],
-    })
-)
+app.use(cors());
+
 
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
